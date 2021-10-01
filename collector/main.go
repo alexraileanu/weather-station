2bd0f9c77aabd7d6a4debeaa240e2213762c2e0a
Alex <alexraileanu@me.com>
2021-10-01 09:47:11 +0200

    fix: fixed weird whitespaces added by vscode(?)

--- collector/main.go
+++ collector/main.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-	"os"
+    "os"
 
-	"github.com/alexraileanu/weather-station/http"
+    "github.com/alexraileanu/weather-station/http"
 
-	"github.com/joho/godotenv"
-	"github.com/spf13/cobra"
+    "github.com/joho/godotenv"
+    "github.com/spf13/cobra"
 )
 
 func initEnv(toRun func(usr, pass, host, db string), env string) {
-	err := godotenv.Load(env)
-	if err != nil {
-		panic(err)
-	}
+    err := godotenv.Load(env)
+    if err != nil {
+        panic(err)
+    }
 
-	toRun(os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+    toRun(os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
 }
 
 func main() {
-	var env string
+    var env string
 
-	var rootCmd = &cobra.Command{Use: "serveradmin"}
-	var apiCmd = &cobra.Command{
-		Use:   "api",
-		Short: "Runs the backend API",
-		Run: func(cmd *cobra.Command, args []string) {
-			s := http.SRV{}
-			initEnv(s.Init, env)
-		},
-	}
+    var rootCmd = &cobra.Command{Use: "serveradmin"}
+    var apiCmd = &cobra.Command{
+        Use:   "api",
+        Short: "Runs the backend API",
+        Run: func(cmd *cobra.Command, args []string) {
+            s := http.SRV{}
+            initEnv(s.Init, env)
+        },
+    }
 
-	apiCmd.Flags().StringVarP(&env, "e", "e", "", "Env file")
-	rootCmd.AddCommand(apiCmd)
+    apiCmd.Flags().StringVarP(&env, "e", "e", "", "Env file")
+    rootCmd.AddCommand(apiCmd)
 
-	rootCmd.Execute()
+    rootCmd.Execute()
 }
