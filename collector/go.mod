ea8726d654ab706715b4aca45c2fa9c6a6ec23e5
Alex <alexraileanu@me.com>
2021-10-06 11:16:24 +0200

    feat: add sendgrid to send mails

--- collector/go.mod
+++ collector/go.mod
@@ -5,6 +5,8 @@ go 1.14
 require (
 	github.com/gin-gonic/gin v1.7.4
 	github.com/joho/godotenv v1.4.0
+	github.com/sendgrid/rest v2.6.5+incompatible // indirect
+	github.com/sendgrid/sendgrid-go v3.10.1+incompatible
 	github.com/spf13/cobra v1.2.1
 	gorm.io/driver/mysql v1.1.2
 	gorm.io/gorm v1.21.15
--- collector/utl/mail/mail.go
+++ collector/utl/mail/mail.go
@@ -1,5 +1,22 @@
 package mail
 
+import (
+    "bytes"
+    "errors"
+    "fmt"
+    "html/template"
+    "os"
+
+    "github.com/sendgrid/sendgrid-go"
+    "github.com/sendgrid/sendgrid-go/helpers/mail"
+)
+
+var TemplatesFolder = "utl/tpl"
+
+type tpl struct {
+    Battery float64
+}
+
 type Service struct {
 }
 
@@ -8,5 +25,45 @@ func New() *Service {
 }
 
 func (s *Service) SendBatteryMail(battery float64) error {
+    tplData := tpl{
+        Battery: battery,
+    }
+
+    templateFile := fmt.Sprintf("%s/email.gohtml", TemplatesFolder)
+    return s.send(tplData, templateFile)
+}
+
+func (s *Service) send(tplData tpl, tplPath string) error {
+    to := mail.NewEmail(os.Getenv("MAIL_TO_NAME"), os.Getenv("MAIL_TO_EMAIL"))
+    t := template.Must(template.ParseFiles(tplPath))
+
+    buf := bytes.Buffer{}
+    err := t.Execute(&buf, tplData)
+    if err != nil {
+        return err
+    }
+
+    from := mail.NewEmail(os.Getenv("MAIL_FROM_NAME"), os.Getenv("MAIL_FROM_EMAIL"))
+    subject := "Weather battery is running low!"
+    message := mail.NewV3MailInit(from, subject, to, mail.NewContent("text/html", buf.String()))
+
+    // disable click tracking in email
+    trackingSettings := mail.NewTrackingSettings()
+    clickTrackingSettings := mail.NewClickTrackingSetting()
+    clickTrackingSettings.SetEnable(false)
+    clickTrackingSettings.SetEnableText(false)
+    trackingSettings.SetClickTracking(clickTrackingSettings)
+    message.SetTrackingSettings(trackingSettings)
+
+    client := sendgrid.NewSendClient(os.Getenv("SENDGRID_KEY"))
+    r, err := client.Send(message)
+    if err != nil {
+        return err
+    }
+
+    if r.StatusCode != 200 {
+        return errors.New(r.Body)
+    }
+
     return nil
 }
