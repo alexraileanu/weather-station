4cc7c79f3b0697f5d557830e6551a1321fe837c8
Alex <alexraileanu@me.com>
2021-12-14 12:43:55 +0100

    feat: set deep sleep to 30 minutes

--- sensor/main/queue.c
+++ sensor/main/queue.c
@@ -3,7 +3,7 @@
 static const char *TAG = "weather_queue";
 static const char *MQTT_TOPIC = "weather";
 static const char *MQTT_CLIENT_ID = "weather_station";
-static const int64_t WAKEUP_TIME_SEC = 3600;
+static const int64_t WAKEUP_TIME_SEC = 1800;
 
 static void send_weather_data(esp_mqtt_client_handle_t *client, QueueHandle_t queue) {
     int msg_id;
@@ -12,11 +12,9 @@ static void send_weather_data(esp_mqtt_client_handle_t *client, QueueHandle_t qu
         if (xQueueReceive(queue, &(weather), pdMS_TO_TICKS(1000))) {
             char json_msg[90];
             create_weather_message(json_msg, &weather);
-            ESP_LOGI(TAG, "Attempting to send json: %s", json_msg);
             msg_id = esp_mqtt_client_publish(*client, MQTT_TOPIC, json_msg, 0, 1, 0);
-            ESP_LOGI(TAG, "Sent JSON: %s to id: %d", json_msg, msg_id);
 
-            ESP_LOGI(TAG, "Entering deep sleep for, 3600s\n");
+            ESP_LOGI(TAG, "Entering deep sleep for 30 minutes\n");
             ESP_ERROR_CHECK(esp_wifi_stop());
             esp_sleep_enable_timer_wakeup(WAKEUP_TIME_SEC * 1000000);
             esp_deep_sleep_start();
